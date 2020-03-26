b6c0454c23ee230a5625c665f6119d4d5948e47b
Russell Strauss <russellstrauss@gmail.com>
2020-03-26 00:46:14 -0400

    Cleanup, rename vars

--- src/js/components/vr.js
+++ src/js/components/vr.js
@@ -387,31 +387,27 @@ module.exports = function() {
 					samples.push(new BABYLON.Vector3(x, y, z));
 				}
 				var audioCurve = new BABYLON.Curve3(samples);
-
 				var points = audioCurve.getPoints();
 				var path3d = new BABYLON.Path3D(points);
 				var curve = path3d.getCurve();
-				var line = BABYLON.Mesh.CreateLines('curve', curve, scene);
-				
-				//var line2 = BABYLON.Mesh.CreateLines('curve', curve, scene);
-				
+				var waveform = BABYLON.Mesh.CreateLines('curve', curve, scene);
 			});
 		},
 		
 		getAudioSamples: function(audioBuffer) {
-			const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
-			const samples = 1000; // Number of samples we want to have in our final data set
-			const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
-			const filteredData = [];
+			let rawData = audioBuffer.getChannelData(0);
+			let samples = 1000;
+			let blockSize = Math.floor(rawData.length / samples);
+			let samplesArray = [];
 			for (let i = 0; i < samples; i++) {
-				let blockStart = blockSize * i; // the location of the first sample in the block
+				let blockStart = blockSize * i;
 				let sum = 0;
 				for (let j = 0; j < blockSize; j++) {
 					sum = sum + Math.abs(rawData[blockStart + j]); // find the sum of all the samples in the block
 				}
-				filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
+				samplesArray.push(sum / blockSize); // divide the sum by the block size to get the average
 			}
-			return filteredData;
+			return samplesArray;
 		}
 	}
 }
