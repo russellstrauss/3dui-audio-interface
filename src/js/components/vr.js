99051510bec1025edf230711d9d3160caaf6b7ae
Russell Strauss <russellstrauss@gmail.com>
2020-04-07 02:29:41 -0400

    Improve controller input method organization; remove unneeded code

--- src/js/components/vr.js
+++ src/js/components/vr.js
@@ -1,63 +1,10 @@
 module.exports = function() {
 	
 	var canvas, engine, scene, camera, vrHelper;
-	var menuItems = [], activeTool, dragTool, eraseTool, saveTool, resetTool;
+	var menuItems = [], scalingRod = {};
 	var frameCount = 0;
-	var leftController, rightController, selectedMesh, draggedMesh;
+	var leftController, rightController, rightJoystick, leftJoystick, selectedMesh, draggedMesh;
 	var red = new BABYLON.Color3(1, 0, 0), green = new BABYLON.Color3(0, 1, 0), green = new BABYLON.Color3(0, 1, 0), white = new BABYLON.Color3(1, 1, 1), black = new BABYLON.Color3(0, 0, 0);
-	
-	class MenuItemBlock {
-
-		constructor(pt, title) {
-			
-			let self = this;
-			
-			this.position = pt;
-			this.active = false;
-			this.title = title;
-			this.boxSize = .5;
-			this.box = BABYLON.MeshBuilder.CreateBox('MenuItemBlock', {size: this.boxSize}, scene)
-			this.box.position = new BABYLON.Vector3(pt.x, pt.y + this.boxSize/2, pt.z);
-			this.box.material = new BABYLON.StandardMaterial('menuItemMaterial', scene);
-			this.box.isMenu = true;
-			menuItems.push(this);
-			
-			this.plane = BABYLON.MeshBuilder.CreatePlane('plane', {height: 1, width: 1}, scene);
-			
-			this.plane.position = gfx.movePoint(pt, new BABYLON.Vector3(0, this.boxSize + .4, 0));
-			this.line = gfx.createLine(gfx.movePoint(pt, new BABYLON.Vector3(0, this.boxSize + .1, 0)), new BABYLON.Vector3(0, .15, 0), white);
-			
-			let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(this.plane);
-			let label = new BABYLON.GUI.TextBlock();
-			label.text = title;
-			label.color = 'white';
-			label.fontSize = 200;
-			advancedTexture.addControl(label);
-			
-			this.plane.lookAt(new BABYLON.Vector3(0, 0, 0));
-			this.plane.addRotation(0, Math.PI, 0)
-						
-			this.box.getParent = function() {
-				return self;
-			}
-		}
-		
-		setActive() {
-			
-			menuItems.forEach(function(menuItem) {
-				menuItem.setInactive();
-			});
-			
-			this.box.material.emissiveColor = new BABYLON.Color3(.03, .56, .95);
-			this.active = true;
-			activeTool = this;
-		}
-		
-		setInactive() {
-			this.box.material.emissiveColor = new BABYLON.Color3(0, 0, 0);
-			this.active = false;
-		}
-	}
 	
 	return {
 		
@@ -97,10 +44,13 @@ module.exports = function() {
 			camera.position = new BABYLON.Vector3(0, 1.2, -1.1);
 			camera.attachControl(canvas, true);
 			
-			self.addButtonEvents();
+			vrHelper = scene.createDefaultVRExperience();
+			vrHelper.onEnteringVRObservable.add(function() {
+				self.addButtonEvents();
+			});
 			self.setLighting();
-			self.addMenu();
 			self.addDesk();
+			
 			self.showAudioSamples('./src/audio/greenfields.mp3');
 			
 			scene.clearColor = new BABYLON.Color3(0, 0, 0);
@@ -145,243 +95,162 @@ module.exports = function() {
 			
 			var desk = new BABYLON.TransformNode();
 			BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
-			BABYLON.SceneLoader.ImportMesh('', './src/obj/', 'AudioEquipmentTexture.obj', scene, function(meshChildren) {
+			BABYLON.SceneLoader.ImportMesh('', './src/obj/', 'AudioEquipmentTexture.gltf', scene, function(meshChildren) {
 
 				for (let i = 0; i < meshChildren.length; i++) {
 					meshChildren[i].parent = desk;
 				}
 				desk.position.x = 0, desk.position.y = .1, desk.position.z = 0;
-				// desk.rotate(BABYLON.Axis.Y, Math.PI, BABYLON.Space.WORLD);
-				desk.scaling = new BABYLON.Vector3(.045, .045, .045);
+				desk.rotate(BABYLON.Axis.Y, Math.PI, BABYLON.Space.WORLD);
+				desk.scaling = new BABYLON.Vector3(.05, .05, .05);
 			});
-			
-			// var turntable = new BABYLON.TransformNode();
-			// BABYLON.SceneLoader.ImportMesh('', './src/obj/', 'turntable.gltf', scene, function(meshChildren) {
-
-			// 	for (let i = 0; i < meshChildren.length; i++) {
-			// 		meshChildren[i].parent = turntable;
-			// 	}
-			// 	turntable.position.x = 0, turntable.position.y = .5, turntable.position.z = 0;
-			// 	// turntable.rotate(BABYLON.Axis.Y, Math.PI, BABYLON.Space.WORLD);
-			// 	turntable.scaling = new BABYLON.Vector3(.04, .04, .04);
-			// });
-		},
-		
-		addMeshSelectionEvents: function() {
-			
-			let self = this;
-			
-			vrHelper.onControllerMeshLoaded.add(function(webVRController) {
-				
-				if (webVRController.hand === 'left') leftController = webVRController;
-				if (webVRController.hand === 'right') rightController = webVRController;
-				
-				webVRController.onTriggerStateChangedObservable.add(function(stateObject) {
-					
-					if (webVRController.hand === 'right' && selectedMesh != null && stateObject.value >= 1) { // grab
-						
-						if (typeof selectedMesh.getParent === 'function') {
-							selectedMesh.getParent().setActive();
-						}
-					}
-				});
-			});
-			
-			vrHelper.onNewMeshSelected.add(function(mesh) {
-				selectedMesh = mesh;
-			});
-		
-			vrHelper.onSelectedMeshUnselected.add(function() {
-				selectedMesh = null;
-			});
-		},
-		
-		addDragging: function() {
-			
-			vrHelper.onNewMeshSelected.add(function(mesh) {
-				draggedMesh = mesh;
-			});
-		
-			vrHelper.onSelectedMeshUnselected.add(function() {
-				draggedMesh = null;
-			});
-		},
-		
-		addMenu: function() {
-			
-			let self = this;
-			var light = new BABYLON.PointLight('light', new BABYLON.Vector3(0, 2, 0), scene);
-			light.intensity = .2;
-			
-			var boxSize = .5;
-			
-			dragTool = new MenuItemBlock(new BABYLON.Vector3(8, 0, 8), 'Drag');
-			saveTool = new MenuItemBlock(new BABYLON.Vector3(8, 0, -8), 'Save');
-			eraseTool = new MenuItemBlock(new BABYLON.Vector3(-8, 0, -8), 'Erase');
-			resetTool = new MenuItemBlock(new BABYLON.Vector3(0, 0, -8), 'Reset');
 		},
 		
 		addButtonEvents: function() {
 			
 			let self = this;
-
-			vrHelper = scene.createDefaultVRExperience();
 			vrHelper.enableInteractions();
+			vrHelper.enableTeleportation({
+				floorMeshName: 'ground'
+			});
+			self.deactivateTeleportation();
+			vrHelper.teleportationTarget = BABYLON.Mesh.CreateSphere('ground', 4, 0.05, scene);
+			vrHelper._teleportBackwardsVector = new BABYLON.Vector3(0, -.1, -.1);
+			console.log('vrHelper: ', vrHelper);
 			
-			self.addMeshSelectionEvents();
-			self.addDragging();
-
-			const leftHand = BABYLON.Mesh.CreateBox('leftHand', 0.1, scene);
-			leftHand.scaling.z = 2;
-			leftHand.isVisible = false;
-
-			const rightHand = BABYLON.Mesh.CreateBox('rightHand', 0.1, scene);
-			rightHand.scaling.z = 2;
-			rightHand.isVisible = false;
-			// vrHelper.enableTeleportation({
-			// 	floorMeshName: 'ground'
-			// });
-			// vrHelper.teleportationTarget = BABYLON.Mesh.CreateSphere('ground', 4, 0.05, scene);
-			
-			vrHelper.onNewMeshPicked.add(pickingInfo => {
+			vrHelper.onNewMeshPicked.add(pickingInfo => { // where controller is resting/pointing
 				//console.log(pickingInfo); //Callback receiving ray cast picking info
 			});
-
-			vrHelper.onControllerMeshLoaded.add(function(webVRController) {
+			
+			vrHelper.onControllerMeshLoadedObservable.add(function(webVRController) {
+				if (webVRController.hand === 'left') {
+					vrHelper._leftController._laserPointer._isEnabled = false;
+					vrHelper._leftController._gazeTracker._isEnabled = false;
+					leftController = webVRController;
+				}
+				if (webVRController.hand === 'right') {
+					vrHelper._rightController._laserPointer._isEnabled = false;
+					rightController = webVRController;
+				}
 				
-				webVRController.onSecondaryButtonStateChangedObservable.add(function(stateObject) {
-					
-					if (webVRController.hand === 'left') {
-						
-						if (stateObject.pressed === true) {	
-							self.buttonLeftY(webVRController);
+				if (leftController && webVRController === leftController) {
+					leftController.onSecondaryButtonStateChangedObservable.add(function(stateObject) {
+						if (stateObject.pressed) {
+							self.leftY(leftController);
 						}
-					}
-					else {
-						if (stateObject.pressed === true) {
-							self.buttonRightB(webVRController, stateObject);
+					});
+					leftController.onMainButtonStateChangedObservable.add(function(stateObject) {
+						if (stateObject.pressed) {
+							self.leftX(leftController);
 						}
-						
-						if (activeTool === dragTool && draggedMesh !== null && draggedMesh.draggable === true) { // dragging
-							if (stateObject.pressed === true) { // grab
-								webVRController.mesh.addChild(draggedMesh);
-							}
-							else { // ungrab 
-								webVRController.mesh.removeChild(draggedMesh);
-							}
+					});
+					leftController.onTriggerStateChangedObservable.add(function(stateObject) {
+						if (stateObject.value >= 1) {
+							self.leftTrigger(leftController);
 						}
-					}
-				});
+					});
+					leftController.onSecondaryTriggerStateChangedObservable.add(function(stateObject) {
+						if (stateObject.value >= 1) {
+							self.leftSecondaryTrigger(leftController, stateObject);
+						}
+						else { self.leftSecondaryTriggerRelease(); }
+					});
+					leftController.onPadValuesChangedObservable.add(function(stateObject) {
+						leftJoystick = stateObject;
+						if (stateObject.y > 0 || stateObject.y < 0) {
+							self.joystickLeft(leftController, stateObject);
+						}
+						else { self.leftJoystickRelease(); }
+					});
+				}
 				
-				webVRController.onMainButtonStateChangedObservable.add(function(stateObject) {
-					
-					if (webVRController.hand === 'left' && stateObject.pressed === true) {
-						
-						self.buttonLeftX(webVRController);
-					}
-					else if (webVRController.hand === 'right' && stateObject.pressed === true) {
-						
-						self.buttonRightA(webVRController);
-					}
-				});
-
-				webVRController.onTriggerStateChangedObservable.add(function(stateObject) {
-
-					if (webVRController.hand === 'left' && stateObject.value >= 1) {
-						
-						self.buttonLeftBackTrigger(webVRController);
-					}
-					else if (webVRController.hand === 'right' && stateObject.value >= 1) {
-						
-						self.buttonRightBackTrigger(webVRController);
-					}
-				});
-
-				webVRController.onSecondaryTriggerStateChangedObservable.add(function(stateObject) {
-
-					if (webVRController.hand === 'left' && stateObject.value >= 1) {
-
-						self.buttonLeftSideTrigger(webVRController, stateObject);
-					}
-					else if (webVRController.hand === 'right' && stateObject.value >= 1) {
-						
-						self.buttonRightSideTrigger(webVRController, stateObject);
-					}
-				});
-
-				webVRController.onPadValuesChangedObservable.add(function(stateObject) {
-					
-					if (webVRController.hand === 'left') {
-						self.joystickLeft(webVRController, stateObject);
-					}
-					else if (webVRController.hand === 'right') {
-						self.joystickRight(webVRController, stateObject);
-					}
-				});
+				if (rightController && webVRController === rightController) {
+					rightController.onSecondaryButtonStateChangedObservable.add(function(stateObject) {
+						if (stateObject.pressed) {
+							self.rightB(rightController, stateObject);
+						}
+					});
+					rightController.onMainButtonStateChangedObservable.add(function(stateObject) {
+						if (stateObject.pressed) {
+							self.rightA(rightController);
+						}
+					});
+					rightController.onTriggerStateChangedObservable.add(function(stateObject) {
+						if (stateObject.value >= 1) {
+							self.rightTrigger(rightController, stateObject);
+						}
+						else { self.rightTriggerRelease(); }
+					});
+					rightController.onSecondaryTriggerStateChangedObservable.add(function(stateObject) {
+						if (stateObject.value >= 1) {
+							self.rightSecondaryTrigger(rightController, stateObject);
+						}
+					});
+					rightController.onPadValuesChangedObservable.add(function(stateObject) {
+						rightJoystick = stateObject;
+						if (stateObject.y > 0 || stateObject.y < 0) {
+							self.joystickRight(rightController, stateObject);
+						}
+						else { self.rightJoystickRelease(); }
+					});
+				}
 			});
 		},
 		
-		addColorpicker: function() {
-			
-			let colorpickerSize = 6;
-			var colorpickerPlane = BABYLON.Mesh.CreatePlane('colorpickerPlane', colorpickerSize, scene);
-			colorpickerPlane.material = new BABYLON.StandardMaterial('wallMat', scene);
-			colorpickerPlane.position = new BABYLON.Vector3(0, colorpickerSize/2 + 1, 9);
-			
-			var colorpickerTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(colorpickerPlane, 1024, 1024);
-			
-			var panel = new BABYLON.GUI.StackPanel();
-			panel.width = '1024px';
-			panel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-			panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-			colorpickerTexture.addControl(panel);
-
-			var picker = new BABYLON.GUI.ColorPicker();
-			picker.height = '1000px';
-			picker.width = '1000px';
-			picker.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
-			picker.onValueChangedObservable.add(function(color) {
-				activeColor = color;
-				vrHelper.setLaserColor(activeColor);
-				vrHelper.setLaserLightingState(false);
-			});
-
-			panel.addControl(picker); 
+		leftY: function(webVRController) {
+			console.log('play song');
+			this.playSong('./src/audio/greenfields.mp3', 1);
 		},
-		
-		buttonLeftY: function(webVRController) {},
-		buttonLeftX: function(webVRController) {},
-		buttonRightA: function(webVRController) {},
-		buttonRightB: function(webVRController) {
-			
+		leftX: function(webVRController) {},
+		rightA: function(webVRController) {},
+		rightB: function(webVRController) {},
+		leftTrigger: function(webVRController) {},
+		rightTrigger: function(webVRController, stateObject) {},
+		rightTriggerRelease: function() {},
+		leftSecondaryTrigger: function(webVRController) {
+			this.activateTeleportation();
 		},
-		buttonLeftBackTrigger: function(webVRController) {},
-		buttonRightBackTrigger: function(webVRController, stateObject) {},
-		buttonLeftSideTrigger: function(webVRController) {},
-		buttonRightSideTrigger: function(webVRController, stateObject) {},
+		leftSecondaryTriggerRelease: function() {
+			vrHelper.teleportationTarget.dispose();
+			this.deactivateTeleportation();
+		},
+		rightSecondaryTrigger: function(webVRController, stateObject) {},
 		joystickLeft: function(webVRController, stateObject) {},
+		leftJoystickRelease: function() {},
 		joystickRight: function(webVRController, stateObject) {},
+		rightJoystickRelease: function() {},
+		activateTeleportation: function() {
+			vrHelper.teleportationEnabled = true;
+			vrHelper._rotationAllowed = true;
+			vrHelper.teleportationTarget.dispose();
+			vrHelper.teleportationTarget = BABYLON.MeshBuilder.CreateIcoSphere('teleportationTarget', { diameter: .01, subdivisions: 2 }, scene);
+			if (!vrHelper.teleportationTarget.material) {
+				vrHelper.teleportationTarget.material = new BABYLON.StandardMaterial('teleportationTargetMaterial', scene);
+				vrHelper.teleportationTarget.material.emissiveColor = new BABYLON.Color3.Black;
+			}
+			vrHelper.teleportationTarget.scaling = new BABYLON.Vector3(.2, .2, .2);
+			vrHelper.teleportationTarget.onBeforeRenderObservable.add(function(object) {
+				vrHelper.teleportationTarget.material.alpha = .25;
+			});
+			
+			if (vrHelper.teleportationTarget.material) {
+				vrHelper.teleportationTarget.material.emissiveColor = new BABYLON.Color3.White;
+				vrHelper.teleportationTarget.material.wireframe = true;
+			}
+		},
+		
+		deactivateTeleportation: function() {
+			vrHelper.teleportationEnabled = false;
+			vrHelper._rotationAllowed = false;
+		},
 		
 		setLighting: function() {
 			
-			// var directionalLight = new BABYLON.DirectionalLight('DirectionalLight', new BABYLON.Vector3(0, -1, 0), scene);
-			// directionalLight.diffuse = new BABYLON.Color3(.4, .4, .4);
-			// directionalLight.specular = new BABYLON.Color3(0, 0, .1);
-			
-			// var spotLight = new BABYLON.SpotLight('spotLight', new BABYLON.Vector3(0, 2, 0), new BABYLON.Vector3(0, -1, 0), Math.PI / 3, 2, scene);
-			// spotLight.diffuse = new BABYLON.Color3(1, 1, 1);
-			// spotLight.specular = new BABYLON.Color3(1, 1, 1);
-			
-			var light = new BABYLON.HemisphericLight('HemiLight', new BABYLON.Vector3(0, 1, 0), scene);
-
+			var hemisphericLight = new BABYLON.HemisphericLight('HemiLight', new BABYLON.Vector3(0, 1, 0), scene);
+			hemisphericLight.intensity = .1;
+			var pointLight = new BABYLON.PointLight('pointLight', new BABYLON.Vector3(0, 2, 0), scene);
+			pointLight.intensity = .7;
 			var ground = BABYLON.MeshBuilder.CreateGround('ground', { height: 20, width: 20, subdivisions: 4, isPickable: false }, scene);
-		},
-		
-		showBabylonDebugger: function() {
-			
-			scene.debugLayer.show({
-			    overlay: true
-			});
 		},
 		
 		showAudioSamples: function(url) {
@@ -400,12 +269,12 @@ module.exports = function() {
 				
 				var samples = [];
 				var waveformLength = 3;
-				var reds2 = [];
+				var colors = [];
 				for (var i = 0; i < 1000; i++) {
 					let x = -(waveformLength/2) + ((waveformLength/1000) * i); // the dividing by 2 centers in view, then divide into 1000 chunks to get desired length
 					let y = audioStreamSamples[i] + 2;
 					let z = 3;
-					reds2.push(new BABYLON.Color4(1, 1, 1, 1));
+					colors.push(new BABYLON.Color4(0, 1, 0, 1));
 					samples.push(new BABYLON.Vector3(x, y, z));
 				}
 				var audioCurve = new BABYLON.Curve3(samples);
